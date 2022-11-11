0b3aae3041feae45a0445de8e21350703f285ae0
ITSVAPP <it.da.fukase@iftc.co.jp>
2022-11-11 14:30:49 +0900

    パス修正

--- src/services/handle_message_service.py
+++ src/services/handle_message_service.py
@@ -2,7 +2,7 @@ import json
 
 class handle_message_service :
 	def generate_reply_message(receivedMessage) :
-		json_file = open('/app/src/dict/mydict.json', 'r')
+		json_file = open(file='./src/dict/mydict.json', mode='r', encoding="utf-8")
 		mydict = json.load(json_file)
 		
 		for key in mydict :
